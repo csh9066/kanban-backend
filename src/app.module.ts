6908259b08511a74e44eb729cc416d53a1648c60
seungho <50465901+csh9066@users.noreply.github.com>
2021-04-05 16:54:53 +0900

    Add board module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(), UsersModule, AuthModule, BoardsModule],
   controllers: [],
   providers: [],
 })
