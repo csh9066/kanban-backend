62750667d52358122f0efbee7f9655624e1e669c
seungho <50465901+csh9066@users.noreply.github.com>
2021-04-06 16:01:35 +0900

    Add AuthUser decorator

---
+++ src/users/auth-user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from './user.entity';
+
+export const AuthUser = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user: User = request.user;
+    return data ? user[data] : user;
+  },
+);
