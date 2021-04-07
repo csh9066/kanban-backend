e90e80a51b38a478fc28e6754647fd110a9c5ba7
seungho <50465901+csh9066@users.noreply.github.com>
2021-04-07 20:56:49 +0900

    Update app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { BoardsModule } from './boards/boards.module';
+import { CardsModule } from './cards/cards.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule, AuthModule, BoardsModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    UsersModule,
+    AuthModule,
+    BoardsModule,
+    CardsModule,
+  ],
   controllers: [],
   providers: [],
 })
